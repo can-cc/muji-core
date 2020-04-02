a5a60524bd93d3066841c754971ce7a9f8d7b456
fwchen <me@chenfangwei.com>
2020-04-02 12:53:13 +0800

    initial commit

---
+++ src/main/kotlin/com/chenfangwei/muji/MujiApplication.kt
@@ -0,0 +1,11 @@
+package com.chenfangwei.muji
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MujiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MujiApplication>(*args)
+}
---
+++ src/test/kotlin/com/chenfangwei/muji/MujiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chenfangwei.muji
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MujiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
